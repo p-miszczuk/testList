dffbed1f4b41efb1093efaaf2b818148975015a1
p-miszczuk <piotr.miszczuk@interia.pl>
2019-10-22 13:54:27 +0200

    fetch data

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Item from './components/Items/Item';
 import Buttons from './components/Buttons/Buttons';
 
-const App = () => {
-  return (
-    <View style={styles.body}>
-      <View style={styles.containerList}>
-        <Item />
+class App extends React.Component {
+
+  state = {
+    isLoading: true,
+    data: []
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData = () => {
+    fetch('https://picsum.photos/v2/list')
+    .then(resp => resp.json())
+    .then(resp => {
+      this.setState({
+        data: [...resp],
+        isLoading: false
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  render() {
+    const {isLoading, error} = this.state
+
+    if (isLoading) 
+      return (
+        <View style={styles.containerSpinner}>
+          <ActivityIndicator />
+        </View>
+      )
+
+    if (error) {
+      <View><Text>{error}</Text></View>
+    }
+
+    return (
+      <View style={styles.body}>
+        <View style={styles.containerList}>
+          <Item />
+        </View>
+        <View style={styles.containerButtons}>
+          <Buttons />
+        </View>
       </View>
-      <View style={styles.containerButtons}>
-        <Buttons />
-      </View>
-    </View>
-  );
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -24,6 +63,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     padding: 10,
+  },
+  containerSpinner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   containerList: {
     flex: 1,
